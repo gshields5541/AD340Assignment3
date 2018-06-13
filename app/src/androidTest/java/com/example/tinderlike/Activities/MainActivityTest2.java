3728f68317977e9ca44b8ac01e0590174a6c17ed
gshields5541 <gshields5541@gmail.com>
2018-06-13 15:36:12 -0700

    Added New MainActivity Test 2

--- app/src/androidTest/java/com/example/tinderlike/Activities/MainActivityTest2.java
+++ app/src/androidTest/java/com/example/tinderlike/Activities/MainActivityTest2.java
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -74,15 +75,6 @@ public class MainActivityTest2 {
                         isDisplayed()));
         appCompatEditText.perform(click());
 
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.email),
                         childAtPosition(
@@ -91,7 +83,7 @@ public class MainActivityTest2 {
                                         0),
                                 0),
                         isDisplayed()));
-        appCompatEditText2.perform(replaceText("WillaF@gmail.com"), closeSoftKeyboard());
+        appCompatEditText2.perform(replaceText("willaf@gmail.com"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText3 = onView(
                 allOf(withId(R.id.password),
@@ -103,15 +95,15 @@ public class MainActivityTest2 {
                         isDisplayed()));
         appCompatEditText3.perform(replaceText("123456"), closeSoftKeyboard());
 
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.button), withText("Sign In"),
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.password), withText("123456"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                2),
+                                1),
                         isDisplayed()));
-        appCompatButton2.perform(click());
+        appCompatEditText4.perform(pressImeActionButton());
 
     }
 
